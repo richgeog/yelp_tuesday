e286efc973b8dd97d8e563f1228dd02fe451d6e6
UpstatePedro <piers.m.holden@gmail.com>
2015-09-02 16:55:22 +0100

    Version 1 complete

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -2,13 +2,23 @@ require 'rails_helper'
 require 'spec_helper'
 
 describe Restaurant, type: :model do
-  it { is_expected.to have_many :reviews }
-end
 
-describe Restaurant, :type => :model do
-  it 'is not valid with a name of less than three characters' do
-    restaurant = Restaurant.new(name: "kf")
-    expect(restaurant).to have(1).error_on(:name)
-    expect(restaurant).not_to be_valid
+  context 'relationship with reviews' do
+    it { is_expected.to have_many :reviews }
   end
+
+  context 'validation' do
+    it 'is not valid with a name of less than three characters' do
+      restaurant = Restaurant.new(name: "kf")
+      expect(restaurant).to have(1).error_on(:name)
+      expect(restaurant).not_to be_valid
+    end
+
+    it 'is not valid unless it has a unique name' do
+        Restaurant.create(name: "Moe's Tavern")
+        restaurant = Restaurant.new(name: "Moe's Tavern")
+        expect(restaurant).to have(1).error_on(:name)
+    end
+  end
+
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it 'is invalid if the rating is more than 5' do
+    review = Review.new(rating:10)
+    expect(review).to have(1).error_on(:rating)
+  end
+
 end

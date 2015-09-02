304fbb66c9bfaf2bed91074177b7752535fe3ce1
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-09-02 12:22:27 +0100

    review test written

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -32,4 +32,11 @@ class RestaurantsController < ApplicationController
     redirect_to '/restaurants'
   end
 
+  def destroy
+    @restaurant = Restaurant.find(params[:id])
+    @restaurant.destroy
+    flash[:notice] = 'Restaurant deleted successfully'
+    redirect_to '/restaurants'
+  end
+
 end

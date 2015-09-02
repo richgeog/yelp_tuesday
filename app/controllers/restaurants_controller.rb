d7d151985ec84a54c7097ace8d1c89f99b3e2107
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-09-02 11:27:33 +0100

    showing restaurats, 1 test failing

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -5,16 +5,20 @@ class RestaurantsController < ApplicationController
   end
 
   def new
-
+    @restaurant = Restaurant.new
   end
 
   def create
-    Restaurant.create(restaurant_params)
+    @restaurant = Restaurant.create(restaurant_params)
     redirect_to '/restaurants'
   end
 
   def restaurant_params
     params.require(:restaurant).permit(:name)
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 
 end

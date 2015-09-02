e2a55cba9d8abb6e27a72547aa468bba33e91d6e
UpstatePedro <piers.m.holden@gmail.com>
2015-09-02 12:03:40 +0100

    Editing & updating restaurants - all tests passing

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -34,12 +34,25 @@ feature 'restaurants' do
     end
 
     context 'viewing restaurants' do
-      let(:kfc){Restaurant.create(name:'KFC')}
+      let!(:kfc){Restaurant.create(name:'KFC')}
       scenario 'lets a user view a restaurant' do
         visit '/restaurants'
         click_link 'KFC'
         expect(page).to have_content 'KFC'
-        expect(current_path).to eq '/restaurants/#{kfc.id}'
+        expect(current_path).to eq "/restaurants/#{kfc.id}"
+      end
+    end
+
+    context 'editing restaurants' do
+      before {Restaurant.create name: 'KFC'}
+
+      scenario 'let a user edit a restaurant' do
+        visit '/restaurants'
+        click_link 'Edit KFC'
+        fill_in 'Name', with: 'Kentucky Fried Chicken'
+        click_button 'Update Restaurant'
+        expect(page).to have_content 'Kentucky Fried Chicken'
+        expect(current_path).to eq '/restaurants'
       end
     end
 

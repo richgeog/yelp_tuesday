0061831bb2c4e3d04f9c6181351fcabf16aaa379
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-10-26 15:24:59 +0000

    added relationships and tests for endorsements

---
+++ spec/factories/endorsements.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :endorsement do
+    review nil
+  end
+
+end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Review, type: :model do
 
   it { is_expected.to belong_to :user }
-  it { is_expected.to belong_to :restaurant   }
+  it { is_expected.to belong_to :restaurant }
+  it { is_expected.to have_many :endorsements }
 
   it 'is invalid if the rating is more than 5' do
     review = Review.new(rating:10)

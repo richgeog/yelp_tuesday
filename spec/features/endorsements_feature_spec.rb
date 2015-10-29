648bb1e1a1c1c09342f492eb2212a11137c08544
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-10-29 21:17:28 +0000

    still unable to fix failing test for images

--- spec/features/endorsements_feature_spec.rb
+++ spec/features/endorsements_feature_spec.rb
@@ -1,23 +1,23 @@
 require 'rails_helper'
 
   feature 'endorsing reviews' do
-#   before do
-#     kfc = Restaurant.create(name: 'KFC')
-#     kfc.reviews.create(rating: 3, thoughts: 'It was an abomination')
-#   end
+    before(:each) do
+      user = build(:user)
+      sign_up(user)
+      visit '/restaurants/new'
+      fill_in 'Name', with: 'kfc'
+      attach_file('restaurant[image]', 'spec/features/images/kfc.jpg')
+      click_button 'Create Restaurant'
+      click_link 'Review kfc'
+      fill_in 'Thoughts', with: 'Nice'
+      select '4', from: 'Rating'
+      click_button 'Leave Review'
+    end
 
   scenario 'a user can endorse a review, which updates the reviews endorsement count', js: true do
-    user = build(:user)
-    sign_up(user)
-    visit '/restaurants/new'
-    # click_link 'Add a restaurant'
-    fill_in 'Name', with: 'kfc'
-    attach_file('restaurant[image]', 'spec/features/images/kfc.jpg')
-    click_button 'Create Restaurant'
-    expect(page).to have_content 'kfc.jpg'
-    expect(current_path).to eq '/restaurants'
     visit '/restaurants'
     click_link 'Endorse'
     expect(page).to have_content('1 Endorsement')
   end
 end
+

3ecfc59e590ceb15586d33b59ca01a2426500dde
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2015-10-30 12:05:47 +0000

    changed aws info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,7 +30,7 @@ Rails.application.configure do
       :access_key_id => ENV["AWS_ACCESS_KEY_ID"],
       :secret_access_key => ENV["AWS_SECRET_ACCESS_KEY"],
       :path =>'/:class/:attachment/:id_partition/:style/:filename',
-      :s3_host_name => 's3-us-west-2.amazonaws.com'
+      :s3_host_name => 's3-eu-west-1.amazonaws.com'
     }
   }
 end

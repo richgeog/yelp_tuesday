770db3e64e867ead159c8c59e76edaf8b17c986c
Richard Geoghegan <richard.geoghegan@ntlworld.com>
2016-02-29 18:08:05 +0000

    refactored reviews controller and tidied up routes in config

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,31 +1,40 @@
 class ReviewsController < ApplicationController
-
   def new
-    @restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant = restaurant
     @review = Review.new
   end
 
   def create
-    @restaurant = Restaurant.find(params[:restaurant_id])
+    @restaurant = restaurant
     @review = @restaurant.reviews.build_with_user(review_params, current_user)
     if @review.save
       redirect_to restaurants_path
     else
-      if @review.errors[:user]
-        redirect_to restaurants_path, alert: 'You have alredy reviewed this restaurant'
-      else
-        render :new
-      end
+      check_user_reviewed
     end
-  end
-
-  def review_params
-    params.require(:review).permit(:thoughts, :rating)
   end
 
   def destroy
     @review = Review.find(params[:id])
     @review.destroy
     redirect_to restaurants_path
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:thoughts, :rating)
+  end
+
+  def restaurant
+    @_restaurant ||= Restaurant.find(params[:restaurant_id])
+  end
+
+  def check_user_reviewed
+    if @review.errors[:user]
+      redirect_to restaurants_path, alert: 'You have already reviewed this restaurant'
+    else
+      render :new
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
-  root to: "restaurants#index"
+  root to: 'restaurants#index'
 
   resources :users do
     resources :restaurants
   end
 
   resources :restaurants, shallow: true do
-    resources :reviews do
-      resources :endorsements
+    resources :reviews, only: [:new, :create, :destroy] do
+      resources :endorsements, only: [:create]
     end
   end
 end
